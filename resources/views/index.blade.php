aa0f986a18dc8d00ea1084a4a3a097b95c5bbaca
ciprian <ciprianvlad888@gmail.com>
2024-10-28 06:23:03 +0000

    setup contact form logic

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,7 @@
     <script src="https://kit.fontawesome.com/1ce7f964f6.js" crossorigin="anonymous"></script>
     <title>Document</title>
     @vite('resources/css/app.css')
-
+    
 </head>
 <body class="bg-gray-800">
     <header class="lg:grid grid-cols-3 items-center gap-2 py-10 lg:grid-cols-3 flex flex-col-reverse">
@@ -19,68 +19,67 @@
         </div>
         <!-- Login Logic -->
         @if (Route::has('login'))
-            <nav class="flex flex-1 justify-end mx-auto">
-                @auth
-                    <a
-                        href="{{ url('/dashboard') }}"
-                        class="rounded-md px-3 py-2 text-primary hover:bg-support hover:text-white text-center  outline-dotted duration-300 text-2xl"
-                    >
-                        Dashboard
-                    </a>
-                @else
-                    <a
-                        href="{{ route('login') }}"
-                        class="rounded-md px-3 py-2 text-primary hover:bg-support hover:text-white text-center  outline-dotted duration-300 mx-6"
-                    >
-                        Log in
-                    </a>
-
-                    @if (Route::has('register'))
-                        <a
-                            href="{{ route('register') }}"
-                            class="rounded-md px-3 py-2 text-primary hover:bg-support hover:text-white text-center  outline-dotted duration-300"
-                        >
-                            Register
-                        </a>
-                    @endif
-                @endauth
-            </nav>
-        @endif
-
-    </header>
-<a href="#discover" id="scroll-down" class="duration-300 fixed bottom-5 right-4 text-3xl mr-6 animate-bounce hover:animate-none"> 
+        <nav class="flex flex-1 justify-end mx-auto">
+            @auth
+            <a
+            href="{{ url('/dashboard') }}"
+            class="rounded-md px-3 py-2 text-primary hover:bg-support hover:text-white text-center  outline-dotted duration-300 text-2xl"
+            >
+            Dashboard
+        </a>
+        @else
+        <a
+        href="{{ route('login') }}"
+        class="rounded-md px-3 py-2 text-primary hover:bg-support hover:text-white text-center  outline-dotted duration-300 mx-6"
+        >
+        Log in
+    </a>
+    
+    @if (Route::has('register'))
+    <a
+    href="{{ route('register') }}"
+    class="rounded-md px-3 py-2 text-primary hover:bg-support hover:text-white text-center  outline-dotted duration-300"
+    >
+    Register
+</a>
+@endif
+@endauth
+</nav>
+@endif
+</header>
+<a href="#discover" id="scroll-down" class="duration-300 fixed bottom-8 right-4 text-3xl mr-6 animate-bounce hover:animate-none"> 
     <i class="fa-solid fa-gift text-secondary hover:text-tertiary fa-2xl"></i>
 </a>
-    <!-- About Section -->
-    <section id="about-us" class="about-section text-secondary">    
-        <article class="mt-16">
-            <div class="flex flex-col mx-auto w-fit">
+<!-- About Section -->
+<section id="about-us" class="about-section text-secondary">    
+    <article class="mt-16">
+        <div class="flex flex-col mx-auto w-fit">
             <div class="flex lg:flex-row-reverse flex-col items-center gap-12 mx-4 mb-14">
                 <p class="text-3xl leading-loose">
                     Welcome to Agnes' Timeless Treasures, your trusted destination for exquisite second-hand jewelry, and new treasures.<br> With a passion for quality and a love for craftsmanship, we offer a curated selection of beautiful, pre-loved items that tell a story.<br> Whether you're looking for a vintage piece to add to your collection or an elegant accessory to complement your style, we have something unique for everyone.
                 </p>
-                    <img src="{{ asset('/images/green_pearl_necklace.jpg') }}" alt="Crystals Necklace" class="lg:w-1/3 lg:h-[40rem] rounded-3xl">
+                <img src="{{ asset('/images/green_pearl_necklace.jpg') }}" alt="Crystals Necklace" class="lg:w-1/3 lg:h-[40rem] rounded-3xl">
             </div>
             <div class="flex items-center gap-12 mx-4 lg:flex-row flex-col">
                 <p class="text-3xl leading-loose">
                     At Agnes' Timeless Treasures, we believe in giving jewelry a second life.<br> Every item in our store is carefully inspected to ensure authenticity and quality, providing you with the opportunity to own timeless pieces at affordable prices.<br> Our collection is constantly updated, so you never know what hidden gem you'll discover next.
                 </p>
-                    <img src="{{ asset('/images/teal_pearl_necklace.jpg') }}" alt="Crystals Necklace" class="lg:w-1/3 lg:h-[40rem] rounded-3xl">
+                <img src="{{ asset('/images/teal_pearl_necklace.jpg') }}" alt="Crystals Necklace" class="lg:w-1/3 lg:h-[40rem] rounded-3xl">
             </div>
             <div class="flex lg:flex-row-reverse flex-col items-center gap-12 mx-4 mt-10 lg:text-center">
                 <p class="text-3xl leading-loose">
                     Browse our selection online or visit us in-store to find something exceptional. <br> Thank you for choosing Agnes' Timeless Treasures where every piece has a story, and every customer is treated like family.
                 </p>
-                    <img src="{{ asset('/images/unique_hoop_pearl_necklace.jpg') }}" alt="Crystals Necklace" class="lg:w-1/3 lg:h-[40rem] rounded-3xl">
+                <img src="{{ asset('/images/unique_hoop_pearl_necklace.jpg') }}" alt="Crystals Necklace" class="lg:w-1/3 lg:h-[40rem] rounded-3xl">
             </div>
             <a href="{{ route('products') }}" id="discover" class="hover:bg-support text-center text-primary outline-dotted duration-300 lg:w-1/3 mx-auto p-2 my-20 lg:py-4 rounded-md text-2xl">Discover our selection online</a>          
         </div>
     </article>
 </section>
-      </div>
-    </div>
-  </div>
+</div>
+</div>
+</div>
 
-  
+
 </body>
 </html>
